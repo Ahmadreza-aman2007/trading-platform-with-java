9c7cc72cc7587feb06c2d097d2dfd5cd3b3120f0
parsaemadi19 <parsa.18.e.1385@gmail.com>
2026-07-07 18:11:01 +0330

    Add frontend project config, DTO models, and utility classes

---
+++ source/frountend/src/main/java/app/controllers/utils/InputValidator.java
@@ -0,0 +1,70 @@
+package app.controllers.utils;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+
+import java.util.function.UnaryOperator;
+
+public class InputValidator {
+    public static void numberOnly(TextField field) {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("[0-9]*")) {
+                return change;
+            }
+            return null;
+        };
+        field.setTextFormatter(new TextFormatter<>(filter));
+    }
+
+    public static void letterOnly(TextField field) {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("[a-zA-Z]*")) {
+                return change;
+            }
+            return null;
+        };
+        field.setTextFormatter(new TextFormatter<>(filter));
+    }
+
+    public static void letterWithNumber(TextField field) {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("[a-zA-Z0-9]*")) {
+                return change;
+            }
+            return null;
+        };
+        field.setTextFormatter(new TextFormatter<>(filter));
+    }
+
+    public static void letterWithNumberWithDotUnderLine(TextField field) {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("[a-zA-Z0-9._]*")) {
+                return change;
+            }
+            return null;
+        };
+        field.setTextFormatter(new TextFormatter<>(filter));
+    }
+
+    public static void letterWithSpace(TextField field) {
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.matches("[a-zA-Z ]*")) {
+                return change;
+            }
+            return null;
+        };
+        field.setTextFormatter(new TextFormatter<>(filter));
+    }
+
+    public static boolean isThisAPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() != 11) {
+            return false;
+        }
+        return phoneNumber.startsWith("09");
+    }
+}
